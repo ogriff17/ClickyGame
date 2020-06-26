351d609b49e97bdfb3ae4d2c500e11249d391621
Cerridwen <ogriff17@gmail.com>
2020-06-26 17:12:08 -0400

    Some of the commands are not working here aka <wrapper>

---
+++ src/components/navbar/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+function nav (props){
+    return (
+        <nav className = "navbar navbar-dark bg-primary fixed-top">{}
+            <a className = "navbar-brand" href="/"><i className ="fas fa-mouse-pointer mr-3"></i>Clicky Game</a>
+    <li className="navbar nav-item"><h2 className="text-white">{props.message}</h2></li>
+    <li className ="navbar nav-item"><h5 className="text-white>">Score: {props.score} | Top Score: {props.topScore}</h5>
+    </li>
+        </nav>
+    );
+}
+
+export default nav; 
