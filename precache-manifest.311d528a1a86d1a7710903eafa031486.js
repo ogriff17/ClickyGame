f9205283747cd65edaf7f0063586c56bbff375b4
Cerridwen <ogriff17@gmail.com>
2020-06-29 18:13:19 -0400

    Updates

---
+++ precache-manifest.311d528a1a86d1a7710903eafa031486.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f87f55873bfa172602cd511711b0a77",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "84a01014a599ca4768e5",
+    "url": "/clickygame/static/css/main.02c11889.chunk.css"
+  },
+  {
+    "revision": "9c4d0db947cc959a6c16",
+    "url": "/clickygame/static/js/2.15409e2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clickygame/static/js/2.15409e2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84a01014a599ca4768e5",
+    "url": "/clickygame/static/js/main.54d924ac.chunk.js"
+  },
+  {
+    "revision": "7122649522b9616ddb97",
+    "url": "/clickygame/static/js/runtime-main.71d67497.js"
+  }
+]);
